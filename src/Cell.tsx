6c24603507470f572473ded9f78d47278be1dcb8
mshiel04 <mshiel04@eecs.tufts.edu>
2021-12-04 13:03:19 -0500

    feat:face photos/mvp

--- src/Cell.tsx
+++ src/Cell.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import './Cell.css'
 
+import backgroundArr from './images';
+
 interface CellProps {
   value: number
 }
@@ -10,7 +12,9 @@ interface CellState {}
 export default class Cell extends React.Component<CellProps, CellState> {
   render () {
     return (
-      <div className={`cell cell-${this.props.value}`}>
+      <div 
+        className={`cell cell-${this.props.value}`}
+        style={{ backgroundImage:`url(${backgroundArr[Math.log2((this.props.value)/2)]})`, backgroundSize: "cover" }} >
         {this.props.value}
       </div>
     )
---
+++ src/images.js
@@ -0,0 +1,25 @@
+import Background2 from './assets/cyrus-2.png';
+import Background4 from './assets/milo-4.png';
+import Background8 from './assets/dave-8.png';
+import Background16 from './assets/karla-16.png';
+import Background32 from './assets/charlie-32.png';
+import Background64 from './assets/dog-64.png';
+import Background128 from './assets/carmela-128.png';
+import Background256 from './assets/cat-256.png';
+import Background512 from './assets/milocyrus-512.png';
+import Background1024 from './assets/mountain-1024.png';
+import Background2048 from './assets/city-2048.png';
+
+export default [
+    Background2,
+    Background4,
+    Background8,
+    Background16,
+    Background32,
+    Background64,
+    Background128,
+    Background256,
+    Background512,
+    Background1024,
+    Background2048
+];
